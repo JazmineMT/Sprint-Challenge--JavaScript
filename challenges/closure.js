f4d50c897c0a493fc78f41197dc78a16f6f031ad
JazmineMT <jazmineamcginnis@gmail.com>
2020-05-22 08:55:04 -0700

    colsures js is done

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,19 @@ myFunction();
 
 // Explanation: 
 
+// The nested function has the abillity to acces the variable internal because nested function is still in the same scope.Function scope allows us to use any variables anywhere within our function.
+
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function sumation (number){
+    let sumCount = 0;
+    for (let i = 0; i <= number; i++){
+        sumCount += i;
+    }
+  return sumCount;
+};
+
+console.log(sumation(4));
